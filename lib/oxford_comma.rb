78e43beb839e49299bac1a0050c4d86eb0454314
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 04:27:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
   return array.join(" and ") if array.size == 2
-  "#{(array.first array.size - 1).join(',')}, and #{array.last}"
+  "#{(array.first array.size - 1).join(', ')}, and #{array.last}"
 end
